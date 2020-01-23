a1090faa704a6da4be089ba6beb4d9ea764942a3
Vestanu <54820219+Vestanu@users.noreply.github.com>
2020-01-23 23:46:45 +0300

    Задача #1

---
+++ 1.3-functions/main.js
@@ -0,0 +1,41 @@
+//Задание №1
+
+function showSolutionsMessage (a, b, c) {
+    result = getSolution(a, b, c);
+
+    function getSolution(a, b, c) {
+        let d = b * b - 4 * a * c;
+        if (d < 0) {
+            return {D: d};
+        } else if (d == 0) {
+            let x1 = -b / 2 * a;
+            return { roots: [x1], D: d };
+        } else {
+            let x1 = (-b + Math.sqrt(d)) / 2 * a;
+            let x2 = (-b - Math.sqrt(d)) / 2 * a;
+            return { roots: [x1, x2], D: d };
+        }
+    };
+
+    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+    console.log(`Значение дискриминанта: ${result.D}`);
+
+    if (result.D < 0) {
+        console.log(`Уравнение не имеет вещественных корней`);
+    } else if (result.D == 0) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+    } else {
+        console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+    }
+
+}
+
+showSolutionsMessage(1, 2, 3);
+showSolutionsMessage(7, 20, -3);
+showSolutionsMessage(2, 4, 2);
+
+
+//Задание №2
+
+
+//Задание №3
