2c259a0665120b99ff67b1ba2f36a11188d2abed
Tatiana Penkova <tatianapenkova@gmail.com>
2020-01-27 23:18:32 +0300

    Task 2

--- 1.3-functions/main.js
+++ 1.3-functions/main.js
@@ -38,5 +38,67 @@ showSolutionsMessage(2, 4, 2);
 
 //Задание №2
 
+let data = {
+    "algebra": [2, 4, 5, 3, 4],
+    "geometry": [4, 5, 5, 5, 4],
+    "russian": [3, 4, 4, 4, 3],
+    "psychics": [4, 5, 3, 3, 4],
+    "music": [5, 5, 5, 4, 3],
+    "english": [4, 4, 5, 4, 3],
+    "poetry": [5, 3, 4, 5, 3],
+    "chemistry": [4, 5, 2, 3, 3],
+    "french": [3, 5, 2, 3, 4]
+    };
+    
+    
+    function getAverageScore (data) {
+        let vals = [];
+        
+        let result = [];
+        for( let key in data ) {
+            if ( data.hasOwnProperty(key) ) {
+                vals.push(data[key]);
+            }
+        }
+       
+        for (let i = 0; i < vals.length; i++){
+          let sum = 0;
+          let total;
+          for (let j = 0; j < vals[i].length; j++) {
+            sum += vals[i][j];
+        
+          }
+          total = sum /  vals[i].length;
+          result.push(total);
+        }
+    
+      let average = 0;
+      let averTotal = 0;
+      for (let i = 0; i < result.length; i++) {
+        average += result[i] ;
+        averTotal = average / result.length;
+      }
+      result.push(averTotal);
+      
+      function getAverageMark() {
+       data["average"] = "some number" ;
+        let objArray = {};
+        let subjArray = [];
+        let keys = Object.keys(data);
+        for (let i = 0, l = keys.length; i < l; i++) {
+         subjArray.push(keys[i])
+       }
+    
+        for(let i = 0; i< result.length; i++) {
+          objArray[subjArray[i]] = result[i];
+        }
+        return objArray;
+      }
+    
+     return getAverageMark();
+    }
+    
+    
+    console.log(getAverageScore(data));
 
 //Задание №3
